e0f57aee1f0300005e83b857b8a0332e78e7daff
Andre Brutus <andre@wepay.com>
2014-11-21 16:12:36 -0800

    add empoint for json sunseT

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $app->get('/friday/:zip', function ($zip) use ($app) {
     }
 
 });
-$app->get('/sunset/:zip(/:date)', function ($zip, $date = null) use ($app) {
+$app->get('/sunset/:zip(/:date)', $sunset = function ($zip, $date = null) use ($app) {
     $app->response->headers->set('Content-Type', 'text/xml');
     try {
         $location = findZip($zip);
@@ -52,6 +52,37 @@ $app->get('/sunset/:zip(/:date)', function ($zip, $date = null) use ($app) {
     catch(Exception $e) {
         echo '<xml error="500"/>';
         $app->response()->status(500);
+    }
+});
+
+$app->get('/json/sunset(/:zip)', function ($zip = 94306, $date=NULL)  use ($app) {
+    $app->response->headers->set('Content-Type', 'application/json');
+    try {
+        $location = findZip($zip);
+        if(!$location) throw new Exception("Couldn't find!");
+    } catch(Exception $e) {
+        $app->response()->status(404);
+        return;
+    }
+    try {
+        list($lat, $long, $tz) = [$location[2], $location[3], $location[5]];
+        $ts = ($date) ? strtotime(join("/", str_split($date, 2))) : $_SERVER['REQUEST_TIME'];
+        $sun_info = date_sun_info($ts, $lat, $long);
+        $xml = new SimpleXMLElement('<xml/>');
+        $data =  array_combine(['city','state', 'lat', 'long', 'offset' , 'timezone'], $location);
+        foreach ($data as $key => $val) { $node = $xml->addChild($key, $val); }
+        // Convert Unix timestamps returned from date_sum_info to dates
+        foreach ($sun_info as $key => $val) {
+            $dt = new DateTime("@$val", new DateTimeZone('UTC'));
+            $dt->setTimeZone(new DateTimeZone($tz));
+            $node = $xml->addChild($key, $dt->format('c'));
+            $sun_info[$key] = $dt->format('h:i:s A');
+        }
+        echo json_encode($sun_info);
+    }
+    catch(Exception $e) {
+        $app->response()->status(500);
+        print_r($e);
     }
 });
 $app->run();
